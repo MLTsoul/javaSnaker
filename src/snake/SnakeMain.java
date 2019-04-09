ef0465c44e0e3560af383baf6d4a62438cdeed10
LT <704885881@qq.com>
2019-04-09 21:51:30 +0800

    创建项目

---
+++ src/snake/SnakeMain.java
@@ -0,0 +1,14 @@
+package snake;
+
+import snake.ui.MainWindow;
+
+/**
+ * @Date 2019/4/3 21:52
+ * @Created by mlt
+ * @Description
+ */
+public class SnakeMain {
+    public static void main(String[] args){
+        new MainWindow().setVisible(true);
+    }
+}
